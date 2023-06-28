3b3586473003750bea2707eb08c28e5ed41a1f8f
Reysaliot <kodegomd3p.rey.saliot@gmail.com>
2023-06-29 05:29:26 +0800

    slambook

--- app/src/main/java/com/example/myactivities/FirstFragment.kt
+++ app/src/main/java/com/example/myactivities/FirstFragment.kt
@@ -1,11 +1,18 @@
 package com.example.myactivities
 
+//import mu.KotlinLogging
 import android.os.Bundle
+import android.util.Log
+import android.widget.*
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.core.widget.NestedScrollView
 import androidx.navigation.fragment.findNavController
+import com.airbnb.lottie.LottieAnimationView
+import com.example.myactivities.R
 import com.example.myactivities.databinding.FragmentFirstBinding
 
 /**
@@ -32,8 +39,16 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val readingDog     = view.findViewById<LottieAnimationView>(R.id.reading_dog)
+        val progressSample = view.findViewById<ProgressBar>(R.id.progress_sample)
+        val scrollSample   = view.findViewById<NestedScrollView>(R.id.scroll_sample)
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        scrollSample.visibility=View.VISIBLE
+        readingDog.setOnClickListener {
+            readingDog.visibility = View.GONE
+            progressSample.visibility =View.GONE
         }
     }
 
